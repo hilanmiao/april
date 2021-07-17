fa64a8344c99a193a4fde965eb88167e1a690e00
DESKTOP-FI7JHAB\13715 <hilanmiao@126.com>
2021-07-17 11:17:21 +0800

    no message

---
+++ web/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import './styles/element-variables.scss'
+
+import './styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+import bootstrap from './core/bootstrap'
+
+import './core/use' // vue use
+import './icons' // icon
+import './permission' // permission control
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  // init localstorage, vuex
+  created: bootstrap,
+  render: h => h(App)
+})
---
+++ web/src/utils/auth.js
@@ -0,0 +1,16 @@
+// 更好的使用 localStorage 和 sessionStorage
+import storage from 'store'
+
+const TokenKey = 'admin_auth_token'
+
+export function getToken() {
+  return storage.get(TokenKey)
+}
+
+export function setToken(token) {
+  return storage.set(TokenKey, token)
+}
+
+export function removeToken() {
+  return storage.remove(TokenKey)
+}

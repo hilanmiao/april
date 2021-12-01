f4d8dc5c346bf13999130118986b4cf9ba454c14
hilanmiao <hilanmiao@126.com>
2021-12-01 21:28:42 +0800

    refactor: 重构配置的结构

--- server/config/config.local.js
+++ server/config/config.local.js
@@ -4,8 +4,6 @@
 
 const path = require('path');
 
-// 系统公共配置项目
-const sysConfig = require('./config_sys');
 // 根据不同环境导入不同配置
 const envConfig = require('./config_env_local');
 
@@ -19,13 +17,6 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1626422927421_6940';
-
-  // 打开前置代理模式
-  config.proxy = true
-  config.maxProxyCount = 1;
-
   // 集群
   config.cluster = {
     listen: {
@@ -35,55 +26,9 @@ module.exports = appInfo => {
     },
   };
 
-  // 日志
-  config.logger = {
-    outputJSON: true
-  }
-
   // 全局设置响应头
   config.globalHeader = {
     'Powered-by': envConfig.name,
-  };
-
-  // 模板配置
-  config.view = {
-    root: [
-      path.join(appInfo.baseDir, 'app/view'),
-      path.join(appInfo.baseDir, 'path/to/another'),
-    ].join(','),
-    mapping: {
-      '.nj': 'nunjucks',
-    },
-    defaultViewEngine: 'nunjucks', // 默认模板引擎
-    defaultExtension: '.nj', // 省略.nj后缀名
-  };
-
-  //  关闭csrf
-  config.security = {
-    csrf: {
-      enable: false,
-      ignoreJSON: true,
-    },
-    // 白名单
-    domainWhiteList: ['*'],
-  };
-
-  // 允许跨域
-  config.cors = {
-    credentials: true,
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
-  };
-
-  // 上传文件配置
-  config.multipart = {
-    // mode: 'file',  //file模式上传  默认是流的形式上传
-    fileExtensions: ['.rar', '.7z'], // 添加不在默认白名单中的文件类型
-    fileSize: '100mb'
-  };
-
-  // 配置表单提交的最大限制
-  config.bodyParser = {
-    jsonLimit: '10mb'
   };
 
   // sequelize
@@ -123,11 +68,7 @@ module.exports = appInfo => {
     },
   };
 
-  // 添加你的全局中间件(路由的单独中间件顺序在全局之后)
-  config.middleware = ['errorHandler'];
-
   // 其他配置
-  config.sysConfig = sysConfig
   config.envConfig = envConfig
 
   return {
